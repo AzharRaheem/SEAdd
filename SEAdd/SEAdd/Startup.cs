a9d9d2a71f33b112e822c82d00d2abeb01c76cc7
Azhar Raheem <azharraheem992@gmail.com>
2021-11-11 10:17:35 +0000

    Initial commit

---
+++ SEAdd/SEAdd/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SEAdd.Startup))]
+namespace SEAdd
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
